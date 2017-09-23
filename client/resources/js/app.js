30f736c2b5991d86ccd2589ad04265c0332caec3
Leonel Castañeda <leodcastaneda@gmail.com>
2017-09-23 18:21:50 +0000

    prevent search submit

--- client/resources/js/app.js
+++ client/resources/js/app.js
@@ -2,4 +2,19 @@ $(function(){
     $('[data-toggle=confirmation]').confirmation({
         rootSelector: '[data-toggle=confirmation]'
     });
+    
+    // prevent submit 
+    $("#search").keydown(function(event){
+        if(event.keyCode == 13) {
+          event.preventDefault();
+          return false;
+        }
+    });
+    
+    // movil
+    $("#search").keyup(function(event){
+        if(""==event.target.value){
+            $(".form-map").removeClass("active");
+        }
+    });
 });
