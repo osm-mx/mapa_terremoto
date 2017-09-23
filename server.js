45587fbc23d9bb7f130f259d7e82035f9340c04e
Leonel Castañeda <leodcastaneda@gmail.com>
2017-09-23 18:07:13 +0000

    remove automatic updates

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
-var schedule = require('node-schedule');
 
 //setup
 app.set('port', process.env.PORT || 22345);
 app.use(express.static(path.join(__dirname, 'client')));
 
-
-schedule.scheduleJob('59 * * * *', function(){
+/*
+require('node-schedule').scheduleJob('59 * * * *', function(){
     require('./update').updateCrowdData();
 });
-
+*/
 
 //start
 http.listen(app.get('port'), function(){
